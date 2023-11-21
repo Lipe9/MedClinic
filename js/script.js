19caf2ed9817b798b70aba4cbc417d8a20c3cd11
Fellipe ツ <antonio.pereira312@aluno.ce.gov.br>
2023-11-21 20:17:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,4 +15,15 @@ botaoMenu.onclick = function abrirOuFecharMenu() {
   }
 }
 
+function atualizarResultado() {
+  const menu1 = document.getElementById('menu1').value;
+  const menu2 = document.getElementById('menu2').value;
+  const menu3 = document.getElementById('menu3').value;
 
+  const resultado = document.getElementById('itemSelecionado');
+  resultado.textContent = `Selecionado: ${menu1}, ${menu2}, ${menu3}`;
+}
+
+document.getElementById("botaoEnviar").addEventListener("click", function() {
+  alert("Seu formulario foi enviado com sucesso!!");
+});
