8f668dd8001f234cf9489fdf3c5fbbcc75fc0acd
Fellipe ツ <antonio.pereira312@aluno.ce.gov.br>
2023-12-02 21:45:10 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -10,3 +10,43 @@ loginButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("login-form");
+  
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
+    const username = document.getElementById("nm-usuario").value;
+    const password = document.getElementById("senha").value;
+  
+    if (username !== "" && password !== "") {
+      window.location.href = "index.html"; 
+    } else {
+  
+      alert("Preencha todos os campos!");
+  
+      }
+    });
+  });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("rg-form");
+    
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+    
+      const username = document.getElementById("nm-usuario").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("senha").value;
+    
+      if (username !== "" && email != "" && password !== "") {
+        window.location.href = "index.html"; 
+      } else {
+    
+        alert("Preencha todos os campos!");
+    
+        }
+      });
+    });
+    
