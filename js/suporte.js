0831e52d7caf9c31a79646f7f414fb56cb9d7c82
Fellipe ツ <antonio.pereira312@aluno.ce.gov.br>
2023-12-10 12:34:35 -0300

    Add files via upload

---
+++ js/suporte.js
@@ -0,0 +1,7 @@
+document.getElementById('support-icon').addEventListener('click', function() {
+    document.getElementById('support-form').style.display = 'block';
+});
+
+document.getElementById('close-form').addEventListener('click', function() {
+    document.getElementById('support-form').style.display = 'none';
+});
